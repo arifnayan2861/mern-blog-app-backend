6734dda99e5d7d85cbef3e3193db035c24bb93b5
arifnayan2861 <am.nayan2861@gmail.com>
2023-12-10 00:09:47 +0600

    delete comment functionality

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -4,10 +4,14 @@ const router = express.Router();
 import {
   createComment,
   updateComment,
+  deleteComment,
 } from "../controllers/commentControllers.js";
 import { authGuard } from "../middleware/authMiddleware.js";
 
 router.post("/", authGuard, createComment);
-router.put("/:commentId", authGuard, updateComment);
+router
+  .route("/:commentId")
+  .put(authGuard, updateComment)
+  .delete(authGuard, deleteComment);
 
 export default router;
