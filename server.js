5b020947529bb94dec7d7d61582f1cbd833495a4
arifnayan2861 <am.nayan2861@gmail.com>
2023-12-12 08:54:07 +0600

    post categories routes & controllers

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ import {
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
+import postCategoriesRoutes from "./routes/postCategoriesRoutes.js";
 
 dotnev.config();
 
@@ -34,6 +35,7 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/post-categories", postCategoriesRoutes);
 
 // static assets
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
