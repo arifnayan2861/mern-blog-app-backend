3925527fc76f68516e4511d223afb747de27dd0b
arifnayan2861 <am.nayan2861@gmail.com>
2023-12-26 22:10:34 +0600

    final commit

--- controllers/postController.js
+++ controllers/postController.js
@@ -108,6 +108,10 @@ const getPost = async (req, res, next) => {
         select: ["avatar", "name"],
       },
       {
+        path: "categories",
+        select: ["title"],
+      },
+      {
         path: "comments",
         match: {
           check: true,
